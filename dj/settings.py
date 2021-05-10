01aaeb43665e487aa9501a70aba86a58f7744299
shreyas73 <shreyas73k@gmail.com>
2021-05-10 12:04:59 +0530

    Project update

--- dj/settings.py
+++ dj/settings.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import os
 import environ
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -125,3 +126,5 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = "todolist"
 LOGIN_URL = "login"
+
+django_heroku.settings(locals())
