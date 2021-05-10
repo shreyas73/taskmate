ae7adc0956b91787026d4798bc9185767f15ab45
shreyas73 <shreyas73k@gmail.com>
2021-05-10 11:41:04 +0530

    Upload Taskmate project

---
+++ users/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, redirect
+from .forms import CustomRegistrationForm
+from django.contrib import messages
+
+
+def register(request):
+    if request.method == "POST":
+        register_form = CustomRegistrationForm(request.POST)
+        if register_form.is_valid():
+            register_form.save()
+            messages.success(request, ("New user account created, Login to get started"))
+            return redirect('register')
+    else:
+        register_form = CustomRegistrationForm()
+    return render(request, 'register.html', {'register_form': register_form})
